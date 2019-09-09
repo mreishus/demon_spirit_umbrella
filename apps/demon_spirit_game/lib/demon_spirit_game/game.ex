0680d6fcb9f31dcf8fe26f244715fafc5418ef52
Matthew Reishus <mreishus@users.noreply.github.com>
2019-09-09 16:30:44 -0500

    Start to fill out game.ex and add some tests

--- apps/demon_spirit_game/lib/demon_spirit_game/game.ex
+++ apps/demon_spirit_game/lib/demon_spirit_game/game.ex
@@ -1,13 +1,110 @@
 defmodule DemonSpiritGame.Game do
-  defstruct board: nil
-  alias DemonSpiritGame.{Game}
+  defstruct board: nil,
+            white_cards: [],
+            black_cards: [],
+            side_card: nil,
+            turn: :white,
+            winner: nil
+
+  alias DemonSpiritGame.{Game, Card}
 
   @doc """
-  Create a new game.
+  Create a new game with random cards.
+
+  Input: None
+  Output: %Game{}
   """
+  @spec new() :: %Game{}
   def new do
-    %Game{board: initial_board()}
+    cards = Card.cards() |> Enum.take_random(5)
+
+    %Game{
+      board: initial_board(),
+      white_cards: cards |> Enum.slice(0, 2),
+      black_cards: cards |> Enum.slice(2, 2),
+      side_card: cards |> Enum.at(4)
+    }
   end
+
+  @doc """
+  Create a new game with cards specified.  Provide a list of
+  5 cards.  They will be assigned in this order:
+  [WHITE, WHITE, BLACK, BLACK, SIDE].
+
+  Input:
+    cards: [%Cards{}].  List should be length 5
+  Output: %Game{}
+  """
+  @spec new(nonempty_list(%Card{})) :: %Game{}
+  def new(cards) when is_list(cards) and length(cards) == 5 do
+    %Game{
+      board: initial_board(),
+      white_cards: cards |> Enum.slice(0, 2),
+      black_cards: cards |> Enum.slice(2, 2),
+      side_card: cards |> Enum.at(4)
+    }
+  end
+
+  @doc """
+  move/3: Move a piece in the game, if possible.
+
+  Input:
+    game: %Game{}
+    from: {x, y} tuple of piece to pick up and move, example: {2, 2} for the center square
+    to: {x, y} tuple of destination, example: {3, 2} to move it right one square
+  Output:
+    {:ok, %Game{}}
+  Output (error)
+    {:error, _}
+  """
+  def move(game, from, to) do
+    case valid_move(game, from, to) do
+      true -> {:ok, game}
+      false -> {:error, :invalid_move}
+    end
+  end
+
+  @doc """
+  valid_mode/3:  Given a game state and a move specified by coordinates, is that move valid?
+
+  Input:
+    game: %Game{}
+    from: {x, y} tuple of piece to pick up and move, example: {2, 2} for the center square
+    to: {x, y} tuple of destination, example: {3, 2} to move it right one square
+  Output: Boolean, is this move valid?
+  """
+  @spec valid_move(%Game{}, {integer(), integer()}, {integer(), integer()}) :: boolean()
+  def valid_move(game, from, to) do
+    valid_move_piece_exists(game, from)
+  end
+
+  @doc """
+  valid_move_piece_exists/2:  Given a game state and a piece to move, does that piece exist and belong
+  to the currently playing player?
+
+  Input:
+    game: %Game{}
+    from: {x, y} tuple of piece to pick up and move, example: {2, 2} for the center square
+  Output:
+    Boolean: Does the from piece exist, and if so, does it belong to the player whose turn it currently is?
+  """
+  @spec valid_move_piece_exists(%Game{}, {integer(), integer()}) :: boolean()
+  def valid_move_piece_exists(game, from) do
+    piece = Map.get(game.board, from)
+    piece != nil && piece.color == game.turn
+  end
+
+  @doc """
+  change_player/1:  Simply flips the ":turn" field of the game between :white and :black.
+  Does not flip if there is a winner.  [Unsure if this is correct, it could set winner to nil?]
+
+  Input: %Game{}
+  Output: %Game{} with :turn flipped, if there is not a winner.
+  """
+  @spec change_player(%Game{}) :: %Game{}
+  def change_player(game = %{winner: winner}) when not is_nil(winner), do: game
+  def change_player(game = %{turn: :white}), do: %{game | turn: :black}
+  def change_player(game = %{turn: :black}), do: %{game | turn: :white}
 
   _ = """
   initial_board: The initial
@@ -20,8 +117,12 @@ defmodule DemonSpiritGame.Game do
   . . . . .
   P P K P P (White)
   ^ {0, 0} (bottom left)
+
+  Input: none.
+  Output: Map of the format %{ {0, 0} => %{type: :pawn, color: :white}, {x, y} => %{type: ..., color: ... }, ... }
   """
 
+  @spec initial_board() :: map()
   defp initial_board do
     white_pawn = %{type: :pawn, color: :white}
     white_king = %{type: :king, color: :white}
---
+++ apps/demon_spirit_game/test/game_test.exs
@@ -0,0 +1,101 @@
+defmodule GameTest do
+  use ExUnit.Case, async: true
+
+  doctest DemonSpiritGame.Game
+  alias DemonSpiritGame.{Game, Card}
+
+  setup do
+    # Static list of cards, use when creating a new game to remove RNG from tests
+    cards = Card.cards() |> Enum.sort_by(fn card -> card.name end) |> Enum.take(5)
+    game = Game.new(cards)
+    %{cards: cards, game: game}
+  end
+
+  describe "new/0" do
+    test "returns a game" do
+      game = Game.new()
+      # Basic sanity check
+      assert game.board
+      assert game.board[{0, 0}]
+      assert game.board[{4, 4}]
+      assert game.board[{4, 4}].color == :black
+      assert game.board[{4, 4}].type == :pawn
+      assert game.board[{2, 4}].type == :king
+      refute game.board[{5, 5}]
+      assert game.turn == :white
+      refute game.winner
+
+      # Correct number of cards in each list field
+      assert length(game.white_cards) == 2
+      assert length(game.black_cards) == 2
+      assert game.side_card
+
+      # Verify none of the cards overlap
+      assert game.white_cards -- game.black_cards == game.white_cards
+      assert game.white_cards -- [game.side_card] == game.white_cards
+      assert game.black_cards -- [game.side_card] == game.black_cards
+    end
+  end
+
+  describe "new/1" do
+    test "returns a game", %{cards: cards} do
+      game = Game.new(cards)
+      # Basic sanity check
+      assert game.board
+      assert game.board[{0, 0}]
+      assert game.board[{4, 4}]
+      assert game.board[{4, 4}].color == :black
+      assert game.board[{4, 4}].type == :pawn
+      assert game.board[{2, 4}].type == :king
+      refute game.board[{5, 5}]
+      assert game.turn == :white
+      refute game.winner
+
+      # Correct number of cards in each list field
+      assert length(game.white_cards) == 2
+      assert length(game.black_cards) == 2
+      assert game.side_card
+
+      # Verify it used the cards we passed in
+      assert game.white_cards -- cards == []
+      assert game.black_cards -- cards == []
+      assert [game.side_card] -- cards == []
+
+      # Verify none of the cards overlap
+      assert game.white_cards -- game.black_cards == game.white_cards
+      assert game.white_cards -- [game.side_card] == game.white_cards
+      assert game.black_cards -- [game.side_card] == game.black_cards
+    end
+  end
+
+  describe "valid_move_piece_exists/2" do
+    test "valid pieces return true", %{game: game} do
+      assert game |> Game.valid_move_piece_exists({0, 0})
+    end
+
+    test "pieces not on board return false", %{game: game} do
+      refute game |> Game.valid_move_piece_exists({2, 2})
+    end
+
+    test "pieces belonging to other player return false", %{game: game} do
+      refute game |> Game.valid_move_piece_exists({4, 4})
+    end
+  end
+
+  describe "change_player/1" do
+    test "changing player on a new game sets it to black", %{game: game} do
+      game = game |> Game.change_player()
+      assert game.turn == :black
+    end
+
+    test "changing player on a new game twice sets it to white", %{game: game} do
+      game = game |> Game.change_player() |> Game.change_player()
+      assert game.turn == :white
+    end
+
+    test "changing player on a won game does not do anything", %{game: game} do
+      game = %{game | turn: :black, winner: :black} |> Game.change_player()
+      assert game.turn == :black
+    end
+  end
+end
