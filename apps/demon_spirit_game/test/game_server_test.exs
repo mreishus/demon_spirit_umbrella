1e47311914b5bdaaef842c9d23b511fa9bd03da5
Matthew Reishus <mreishus@users.noreply.github.com>
2019-09-11 15:41:09 -0500

    GameServer: Add a test for doing a move

--- apps/demon_spirit_game/test/game_server_test.exs
+++ apps/demon_spirit_game/test/game_server_test.exs
@@ -2,7 +2,7 @@ defmodule GameServerTest do
   use ExUnit.Case, async: true
 
   doctest DemonSpiritGame.GameServer
-  alias DemonSpiritGame.{GameServer, Game}
+  alias DemonSpiritGame.{GameServer, Game, Move, Card}
 
   describe "start_link/1" do
     test "spawns a process" do
@@ -54,8 +54,43 @@ defmodule GameServerTest do
   end
 
   describe "move/2" do
-    ## Need a way to generate an initial game state w/o RNG
-    ## To know a move we can test
+    setup do
+      {:ok, boar} = Card.by_name("Boar")
+      {:ok, cobra} = Card.by_name("Cobra")
+      {:ok, dragon} = Card.by_name("Dragon")
+      %{boar: boar, cobra: cobra, dragon: dragon}
+    end
+
+    test "Moving a piece via GameServer", %{cobra: cobra, boar: boar, dragon: dragon} do
+      ## Need a way to generate an initial game state w/o RNG
+      ## To know a move we can test
+      game_name = generate_game_name()
+      assert {:ok, _pid} = GameServer.start_link(game_name, :hardcoded_cards)
+      game = GameServer.state(game_name)
+
+      ## Simple move
+      move = %Move{from: {0, 0}, to: {1, 1}, card: cobra}
+
+      ## Do the move ourself (w/o server)
+      {:ok, game_move_manual} = Game.move(game, move)
+      {:ok, game_move_server} = GameServer.move(game_name, move)
+
+      ## Are the moves the same?
+      assert game_move_manual == game_move_server
+      ## Are they diff from the initial state?
+      refute game == game_move_server
+
+      ## Did the move do what we expect?
+      # Player flipped
+      assert game_move_server.turn == :black
+      # Card rotated
+      assert game_move_server.cards.side == cobra
+      assert boar in game_move_server.cards.white
+      assert dragon in game_move_server.cards.white
+      # Piece Moved
+      refute game_move_server.board |> Map.has_key?({0, 0})
+      assert game_move_server.board |> Map.has_key?({1, 1})
+    end
   end
 
   defp generate_game_name do
