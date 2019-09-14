3fbe8a67761102439a50c0af964477cc448d3a7a
Matthew Reishus <mreishus@users.noreply.github.com>
2019-09-14 17:31:46 -0500

    Notes

--- apps/demon_spirit_web/lib/demon_spirit_web/live/live_game_show.ex
+++ apps/demon_spirit_web/lib/demon_spirit_web/live/live_game_show.ex
@@ -18,10 +18,18 @@ defmodule DemonSpiritWeb.LiveGameShow do
      )}
   end
 
-  def handle_event("click-piece-" <> coords_str, _value, socket) do
+  def handle_event("click-piece-" <> coords_str, _value, socket = %{assigns: assigns}) do
     [{x, ""}, {y, ""}] = coords_str |> String.split("-") |> Enum.map(&Integer.parse/1)
 
     "Clicked on piece: #{x} #{y}" |> IO.inspect(label: "handle_event")
+    assigns |> IO.inspect(label: "assigns")
     {:noreply, assign(socket, deploy_step: "Starting deploy...")}
   end
 end
+
+# Phoenix.PubSub.broadcast(MyApp.PubSub, "sometopic", :some_event)
+# def handle_info(:some_event, socket) ...
+# make the topic something specific to that user/resource/etc
+# and in mount do if connected?(socket), do: Phoenix.PubSub.subscribe(MyApp.PubSub, topic)
+# so if you want to message a user, the topic can be "user:#{user.id}"
+# if it’s to report the result of an upload processing thing, you could do a topic “uploads:#{upload.id}“, etc
