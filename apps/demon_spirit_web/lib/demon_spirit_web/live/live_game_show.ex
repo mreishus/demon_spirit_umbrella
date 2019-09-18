806267e395b1d64e4e77abdc28a7a55173fcba87
Matthew Reishus <mreishus@users.noreply.github.com>
2019-09-18 15:18:07 -0500

    Trying phoenix presence

--- apps/demon_spirit_web/lib/demon_spirit_web/live/live_game_show.ex
+++ apps/demon_spirit_web/lib/demon_spirit_web/live/live_game_show.ex
@@ -1,7 +1,7 @@
 defmodule DemonSpiritWeb.LiveGameShow do
   use Phoenix.LiveView
   require Logger
-  alias DemonSpiritWeb.{Endpoint, GameUIServer, GameView}
+  alias DemonSpiritWeb.{Endpoint, GameUIServer, GameView, Presence}
 
   def render(assigns) do
     GameView.render("live_show.html", assigns)
@@ -9,11 +9,16 @@ defmodule DemonSpiritWeb.LiveGameShow do
 
   def mount(%{game_name: game_name, guest: guest}, socket) do
     topic = topic_for(game_name)
+
     if connected?(socket), do: Endpoint.subscribe(topic)
+    {:ok, _} = Presence.track(self(), topic, guest.id, guest)
+
     state = GameUIServer.sit_down_if_possible(game_name, guest)
     notify(topic)
 
-    socket = assign(socket, game_name: game_name, topic: topic, state: state, guest: guest)
+    socket =
+      assign(socket, game_name: game_name, topic: topic, state: state, guest: guest, users: [])
+
     {:ok, socket}
   end
 
@@ -48,18 +53,41 @@ defmodule DemonSpiritWeb.LiveGameShow do
     "game-topic:" <> game_name
   end
 
-  def handle_info(
-        broadcast = %{topic: broadcast_topic},
-        socket = %{assigns: %{game_name: game_name, topic: topic}}
-      )
-      when broadcast_topic == topic do
-    case broadcast.event do
-      "state_update" ->
-        state = GameUIServer.state(game_name)
-        {:noreply, assign(socket, state: state)}
+  ## Commented out for experiment below
 
-      _ ->
-        {:noreply, socket}
-    end
+  # def handle_info(
+  #       broadcast = %{event: "state_update", topic: broadcast_topic},
+  #       socket = %{assigns: %{game_name: game_name, topic: topic}}
+  #     )
+  #     when broadcast_topic == topic do
+  #   state = GameUIServer.state(game_name)
+  #   {:noreply, assign(socket, state: state)}
+  # end
+
+  ## Experiment: Keep the "topic matching" stuff out of
+  ## Handle_info - is it the case whenever handle_info is called
+  ## we're already assured the topic is correct? I think it might be.
+
+  # Handle incoming "state_updates": Game state has changed
+  def handle_info(
+        broadcast = %{event: "state_update"},
+        socket = %{assigns: %{game_name: game_name, topic: topic}}
+      ) do
+    state = GameUIServer.state(game_name)
+    {:noreply, assign(socket, state: state)}
+  end
+
+  # Handle "presence_diff", someone joined or left
+  def handle_info(e = %{event: "presence_diff"}, socket = %{assigns: %{topic: topic}}) do
+    # e |> IO.inspect()
+
+    users =
+      Presence.list(topic)
+      |> Enum.map(fn {_user_id, data} ->
+        data[:metas]
+        |> List.first()
+      end)
+
+    {:noreply, assign(socket, users: users)}
   end
 end
