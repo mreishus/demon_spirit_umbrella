6cfddb6faa88dc89818d878f91061af2c6971eac
Matthew Reishus <mreishus@users.noreply.github.com>
2019-09-18 19:36:59 -0500

    Move some leex partials into a subdir

--- apps/demon_spirit_web/lib/demon_spirit_web.ex
+++ apps/demon_spirit_web/lib/demon_spirit_web.ex
@@ -31,7 +31,8 @@ defmodule DemonSpiritWeb do
     quote do
       use Phoenix.View,
         root: "lib/demon_spirit_web/templates",
-        namespace: DemonSpiritWeb
+        namespace: DemonSpiritWeb,
+        pattern: "**/*"
 
       # Import convenience functions from controllers
       import Phoenix.Controller, only: [get_flash: 1, get_flash: 2, view_module: 1]
