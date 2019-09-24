f6d52027438a8589d2a74979e4c4188f963743d9
Matthew Reishus <mreishus@users.noreply.github.com>
2019-09-23 20:22:21 -0500

    Redesigning in Tailwind CSS - WIP

---
+++ apps/demon_spirit_web/assets/tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        'black-80': 'rgba(0,0,0,0.8)',
+        'black-70': 'rgba(0,0,0,0.7)',
+        'black-60': 'rgba(0,0,0,0.6)',
+        'black-50': 'rgba(0,0,0,0.5)',
+        'black-40': 'rgba(0,0,0,0.4)',
+      },
+      spacing: {
+        '72': '18rem',
+        '96': '24rem',
+        '128': '32rem',
+      }
+    }
+  }
+}
