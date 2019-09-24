4627f1fa2d396b4a7c507466438f02dccde212e0
Matthew Reishus <mreishus@users.noreply.github.com>
2019-09-24 17:00:56 -0500

    More css improvements

--- apps/demon_spirit_web/lib/demon_spirit_web/views/game_view.ex
+++ apps/demon_spirit_web/lib/demon_spirit_web/views/game_view.ex
@@ -1,3 +1,18 @@
 defmodule DemonSpiritWeb.GameView do
   use DemonSpiritWeb, :view
+
+  def date_to_md(a) do
+    "#{a.month}/#{a.day}"
+  end
+
+  def date_to_hms(a) do
+    "#{a.hour}:#{zero_pad(a.minute)}"
+  end
+
+  @spec zero_pad(Integer, Integer) :: String
+  defp zero_pad(number, amount \\ 2) do
+    number
+    |> Integer.to_string()
+    |> String.rjust(amount, ?0)
+  end
 end
