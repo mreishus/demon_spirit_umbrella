b634bdce58bc728069330387b015e91878a3193f
Matthew Reishus <mreishus@users.noreply.github.com>
2019-09-27 13:39:53 -0500

    More Ecto removal: Remove repo.ex and the db config from prod.secret.exs

--- apps/demon_spirit/lib/demon_spirit/repo.ex
+++
@@ -1,5 +0,0 @@
-defmodule DemonSpirit.Repo do
-  use Ecto.Repo,
-    otp_app: :demon_spirit,
-    adapter: Ecto.Adapters.Postgres
-end
--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -4,17 +4,22 @@
 # remember to add this file to your .gitignore.
 use Mix.Config
 
-database_url =
-  System.get_env("DATABASE_URL") ||
-    raise """
-    environment variable DATABASE_URL is missing.
-    For example: ecto://USER:PASS@HOST/DATABASE
-    """
+##
+## Removed database_url and config of DemonSpirit.Repo,
+## Since the DB is removed -MR 9/27/19
+## 
 
-config :demon_spirit, DemonSpirit.Repo,
-  # ssl: true,
-  url: database_url,
-  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
+# database_url =
+#   System.get_env("DATABASE_URL") ||
+#     raise """
+#     environment variable DATABASE_URL is missing.
+#     For example: ecto://USER:PASS@HOST/DATABASE
+#     """
+
+# config :demon_spirit, DemonSpirit.Repo,
+#   # ssl: true,
+#   url: database_url,
+#   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
 
 secret_key_base =
   System.get_env("SECRET_KEY_BASE") ||
