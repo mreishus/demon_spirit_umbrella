844ccb667dc1c69149f621b4d9d2fb184a4eacad
Matthew Reishus <mreishus@users.noreply.github.com>
2020-01-08 11:17:01 -0600

    Fix bug of "move ding" audio playing every time the clock ticked

--- apps/demon_spirit_web/assets/js/hooks/update_ding.js
+++ apps/demon_spirit_web/assets/js/hooks/update_ding.js
@@ -1,9 +1,15 @@
 let move_audio = new Audio();
-move_audio.src = '/sounds/move.mp3';
+move_audio.src = "/sounds/move.mp3";
+
+let moves = 0;
 
 let UpdateDing = {
   updated() {
-    move_audio.play();
+    let new_moves = this.el.getAttribute("data-moves");
+    if (new_moves != moves) {
+      moves = new_moves;
+      move_audio.play();
+    }
   }
 };
 export default UpdateDing;
