93a520e54f5e78badc55f58b20294fc368741c91
Matthew Reishus <mreishus@users.noreply.github.com>
2020-02-10 11:11:14 -0600

    Bump version -> 1.2.3

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-VERSION=1.2.2 # Bump in layout/app.html.eex too
+VERSION=1.2.3 # Bump in layout/app.html.eex too
 # ^ Todo: Make this one source of truth
 
 # exit when any command fails
