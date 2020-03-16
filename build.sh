8faa62bdfd357d0e239e66935d959db85b514b4a
Matthew Reishus <mreishus@users.noreply.github.com>
2020-03-16 10:02:04 -0500

    Restore mix check to the build script

--- build.sh
+++ build.sh
@@ -6,23 +6,7 @@ VERSION=1.3.0 # Bump in layout/app.html.eex too
 # exit when any command fails
 set -e
 
-# mix check
-# Mix check commented out
-# Router fails after upgrading phoenix - not sure what this is about.
-echo "Please run 'mix check' manually and verify the results."
-echo "This isn't automated because there's a dialyzer error in the "
-echo "phoenix router.  I don't think it's my fault, but I don't "
-echo "know how to fix it either."
-echo " "
-echo "See: https://github.com/phoenixframework/phoenix/issues/3697"
-echo " "
-
-read -p "Are you sure you want to build? " -n 1 -r
-echo    # (optional) move to a new line
-if [[ ! $REPLY =~ ^[Yy]$ ]]
-then
-    [[ "$0" = "$BASH_SOURCE" ]] && exit 1 || return 1 # handle exits from shell or function but don't exit interactive shell
-fi
+mix check
 
 echo "Building..."
 docker build --tag=mreishus/demon-spirit:$VERSION --tag=mreishus/demon-spirit:latest .
