2d9bd3a52fd1f3c1108dd91835f3bfc167e9bfd8
Matthew Reishus <mreishus@users.noreply.github.com>
2023-11-23 14:24:56 -0600

    Remove gettext compiler

--- apps/demon_spirit_web/mix.exs
+++ apps/demon_spirit_web/mix.exs
@@ -11,7 +11,7 @@ defmodule DemonSpiritWeb.MixProject do
       lockfile: "../../mix.lock",
       elixir: "~> 1.5",
       elixirc_paths: elixirc_paths(Mix.env()),
-      compilers: [:phoenix, :gettext] ++ Mix.compilers(),
+      compilers: [:phoenix] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
       deps: deps()
