bab112adf7d6fb88e3885205a4a01330abb298f4
Matthew Reishus <mreishus@users.noreply.github.com>
2023-11-23 20:18:32 -0600

    Fix formatter file

--- .formatter.exs
+++ .formatter.exs
@@ -1,6 +1,6 @@
 [
   import_deps: [:ecto, :ecto_sql, :phoenix],
-  subdirectories: ["apps/*"]
+  subdirectories: ["apps/*"],
   plugins: [Phoenix.LiveView.HTMLFormatter],
   inputs: ["*.{heex,ex,exs}", "{config,lib,test}/**/*.{heex,ex,exs}", "priv/*/seeds.exs"]
 ]
