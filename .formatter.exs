a44a92f43ebe0385e49568420a2c0656eab0954c
Matthew Reishus <mreishus@users.noreply.github.com>
2023-11-23 20:20:07 -0600

    Upgrade phoenix 1.6.16 -> 1.7.10 (#299)
    * Upgrade phoenix 1.6.16 -> 1.7.10
    
    * Fix formatter file

--- .formatter.exs
+++ .formatter.exs
@@ -1,4 +1,6 @@
 [
-  inputs: ["mix.exs", "config/*.exs"],
-  subdirectories: ["apps/*"]
+  import_deps: [:ecto, :ecto_sql, :phoenix],
+  subdirectories: ["apps/*"],
+  plugins: [Phoenix.LiveView.HTMLFormatter],
+  inputs: ["*.{heex,ex,exs}", "{config,lib,test}/**/*.{heex,ex,exs}", "priv/*/seeds.exs"]
 ]
