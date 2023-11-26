666e256f73a4e4ad9f19964f6e2b97340a151c65
Matthew Reishus <mreishus@users.noreply.github.com>
2023-11-26 09:45:18 -0600

    Remove purgecss - no longer used

--- apps/demon_spirit_web/assets/postcss.config.js
+++ apps/demon_spirit_web/assets/postcss.config.js
@@ -1,28 +1,7 @@
-const purgecss = require("@fullhuman/postcss-purgecss")({
-  // Specify the paths to all of the template files in your project
-  content: [
-    "./js/**/*.html",
-    "./js/**/*.jsx",
-    "./js/**/*.js",
-    "./js/**/*.tsx",
-    "./js/**/*.ts",
-    "../lib/demon_spirit_web/templates/**/*.eex",
-    "../lib/demon_spirit_web/templates/**/*.leex",
-    "../lib/demon_spirit_web/templates/**/*.heex",
-    "../lib/demon_spirit_web/views/**/*.ex",
-    "./css/components/*.css",
-    // etc.
-  ],
-  defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
-});
-
-//console.log({ z: process.env.npm_lifecycle_event === "deploy" });
-
 module.exports = {
   plugins: [
     require("postcss-import"),
     require("tailwindcss"),
     require("autoprefixer"),
-    ...(process.env.npm_lifecycle_event === "deploy" ? [purgecss] : []),
   ],
 };
