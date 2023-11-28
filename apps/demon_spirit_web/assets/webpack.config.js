0751ad9fccb9f396b96c9a4e7510968ce3e208b6
Matthew Reishus <mreishus@users.noreply.github.com>
2023-11-27 21:13:50 -0600

    Upgrade to webpack 5

--- apps/demon_spirit_web/assets/webpack.config.js
+++ apps/demon_spirit_web/assets/webpack.config.js
@@ -2,22 +2,22 @@ const path = require("path");
 const glob = require("glob");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const TerserPlugin = require("terser-webpack-plugin");
-const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = (env, options) => ({
   optimization: {
     minimizer: [
       new TerserPlugin({ cache: true, parallel: true, sourceMap: false }),
-      new OptimizeCSSAssetsPlugin({})
-    ]
+      new CssMinimizerPlugin(),
+    ],
   },
   entry: {
-    "./js/app.js": glob.sync("./vendor/**/*.js").concat(["./js/app.js"])
+    "./js/app.js": glob.sync("./vendor/**/*.js").concat(["./js/app.js"]),
   },
   output: {
     filename: "app.js",
-    path: path.resolve(__dirname, "../priv/static/js")
+    path: path.resolve(__dirname, "../priv/static/js"),
   },
   module: {
     rules: [
@@ -25,19 +25,19 @@ module.exports = (env, options) => ({
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader"
-        }
+          loader: "babel-loader",
+        },
       },
       {
         test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, "css-loader", "postcss-loader"]
-      }
-    ]
+        use: [MiniCssExtractPlugin.loader, "css-loader", "postcss-loader"],
+      },
+    ],
   },
   plugins: [
     new MiniCssExtractPlugin({ filename: "../css/app.css" }),
     new CopyWebpackPlugin({
-      patterns: [{ from: "static/", to: "../" }]
-    })
-  ]
+      patterns: [{ from: "static/", to: "../" }],
+    }),
+  ],
 });
